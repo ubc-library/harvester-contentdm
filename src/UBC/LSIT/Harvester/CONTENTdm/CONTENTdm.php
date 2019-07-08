a4e568a53c99c0bdad5ed0b0218e0e0b8bd10621
Rod McFarland <rod.mcfarland@ubc.ca>
2019-07-08 13:02:17 -0700

    remove Slack integration

--- src/UBC/LSIT/Harvester/CONTENTdm/CONTENTdm.php
+++ src/UBC/LSIT/Harvester/CONTENTdm/CONTENTdm.php
@@ -372,53 +372,6 @@
             return $path;
         }
 
-        public function slack ($message, $sendTeaser = false, $room = "random", $icon = ":ghost:")
-        {
-            return 1;
-            $users = [
-                "skhanker", "sean", "kevinho", "schuyberg", "rod"
-            ];
-
-            $users = [
-                "skhanker", "sean", "kevinho"
-            ];
-
-            $messages = [
-                "jus workin it like the rent is due",
-                "you go ingester, parse that body, four for you, you can do this",
-                "open collections is coming",
-                "to be or not to be (parsed), that is the question",
-                "aint even bovvered",
-                "art thou calling me a goodly rotten apple?!",
-                "can't win them all, now can you"
-            ];
-
-            $teaser = "";
-            if ($sendTeaser) {
-                $teaser = " | @" . $users[rand (0, count ($users) - 1)] . " " . $messages[rand (0, count ($messages) - 1)];
-            }
-
-
-            $room = ($room) ? $room : "open-collections";
-            $data = "payload=" . json_encode (
-                    [
-                        "channel"    => "#{$room}",
-                        "text"       => "{$message}{$teaser}",
-                        "icon_emoji" => $icon
-                    ]
-                );
-
-            // You can get your webhook endpoint from your Slack settings
-            $ch = curl_init ("https://hooks.slack.com/services/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");
-            curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt ($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec ($ch);
-            curl_close ($ch);
-
-            return $result;
-        }
-
         public function rodGetWordPositions ($filePath, $w, $h)
         {
             $fh = fopen ($filePath, 'r');
